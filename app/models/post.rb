a233bad179fe004121a6a0c59df0539a815c78a4
An.nA <anna.git3@gmail.com>
2015-02-18 00:52:40 -0800

    style paragraphs, add fonts directory to asset pipeline

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :name, type: String
   field :content, type: String
   # field :keywords, type: Array, default: ['Rome', 'Modern']
 
   validates :name, presence: true, length: {minimum: 2}
   validates :content, presence: true, length: {minimum: 100}
+
+  def date_published
+      created_at.localtime.strftime("%A, %B %-d, %Y at %l:%M %p")
+  end
+
 end
